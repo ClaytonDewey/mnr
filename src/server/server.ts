cbc1597a8892543be6de1064ec63aca2cab944d5
ClaytonDewey <clay@dryadmedia.com>
2023-01-18 15:47:10 -0600

    Setup to use and render ejs

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,17 @@ import express from 'express';
 
 const server = express();
 
+server.use(express.static('dist'));
+
+server.set('view engine', 'ejs')
+
+server.use('/', (req, res) => {
+  res.render('index');
+})
+
 server.listen('8080', '0.0.0.0', () => {
   console.info(`Express server is listening at http://0.0.0.0:8080`)
 })
+
+// Pick back up here @ 8:20
+// https://www.linkedin.com/learning/learning-full-stack-javascript-development-mongodb-node-and-react-15581237/middlewares-and-templating?autoSkip=true&autoplay=true&resume=false&u=74652290
