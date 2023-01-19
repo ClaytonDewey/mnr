1ff115f10cf0cc90102ed4d26faa9595173db5ca
ClaytonDewey <clay@dryadmedia.com>
2023-01-19 13:07:01 -0600

    Update server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,19 +7,20 @@ const server = express();
 
 server.use(express.static('dist'));
 
-server.set('view engine', 'ejs')
+server.set('view engine', 'ejs');
 
 server.use('/', (req, res) => {
   res.render('index', {
-    content: 'EJS is <em>cool</em>'
+    initialContent: 'Loading...',
   });
-})
+});
 
 server.listen(config.PORT, config.HOST, () => {
-  console.info(`Express server is listening at ${config.SERVER_URL}`,
-  `Free Mem: ${os.freemem() / 1024 / 1024}`
+  console.info(
+    `Express server is listening at ${config.SERVER_URL}`,
+    `Free Mem: ${os.freemem() / 1024 / 1024}`,
   );
-})
+});
 
 // Pick back up here @ 8:20
 // https://www.linkedin.com/learning/learning-full-stack-javascript-development-mongodb-node-and-react-15581237/middlewares-and-templating?autoSkip=true&autoplay=true&resume=false&u=74652290
