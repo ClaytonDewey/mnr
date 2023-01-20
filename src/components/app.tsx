d209fba8cb9450435080528fa91f374bb06707f4
ClaytonDewey <clay@dryadmedia.com>
2023-01-20 13:19:11 -0600

    Setup page navigation

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
+import Contest from './contest';
 import ContestList from './contest-list';
 import Header from './header';
 
 const App = ({ initialData }) => {
+  const [page, setPage] = useState('contestList');
+  const [currentContestId, setCurrentContestId] = useState();
+
+  const navigateToContest = (contestId) => {
+    setPage('contest');
+    setCurrentContestId(contestId);
+  };
+
+  const pageContent = () => {
+    switch (page) {
+      case 'contestList':
+        return (
+          <ContestList
+            initialContests={initialData.contests}
+            onContestClick={navigateToContest}
+          />
+        );
+      case 'contest':
+        return <Contest id={currentContestId} />;
+    }
+  };
+
   return (
     <div className="container">
       <Header message="Naming Contests" />
-      <ContestList initialContests={initialData.contests} />
+      {pageContent()}
     </div>
   );
 };
