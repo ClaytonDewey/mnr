a757679aa2b25a418592c1db84369311cafc2f7a
ClaytonDewey <clay@dryadmedia.com>
2023-01-23 19:30:38 -0600

    Handle nav back to root

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -25,6 +25,12 @@ const App = ({ initialData }) => {
     setCurrentContest({ id: contestId });
   };
 
+  const navigateToContestList = () => {
+    window.history.pushState({}, '', `/`);
+    setPage('contestList');
+    setCurrentContest(undefined);
+  };
+
   const pageContent = () => {
     switch (page) {
       case 'contestList':
@@ -35,7 +41,12 @@ const App = ({ initialData }) => {
           />
         );
       case 'contest':
-        return <Contest initialContest={currentContest} />;
+        return (
+          <Contest
+            initialContest={currentContest}
+            onContestListClick={navigateToContestList}
+          />
+        );
     }
   };
 
